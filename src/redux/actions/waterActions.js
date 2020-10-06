985e0ebbdab98f53aafc6391f35abd4fefb91caf
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2020-10-06 17:16:13 +0000

    fix deploy

--- src/redux/actions/waterActions.js
+++ src/redux/actions/waterActions.js
@@ -5,8 +5,8 @@ import {
   WATERS_ERRORS,
 } from './types';
 
-const defaultURL = 'http://localhost:3000';
-// const defaultURL = 'https://mysterious-ravine-52687.herokuapp.com/'; //production
+// const defaultURL = 'http://localhost:3000';
+const defaultURL = 'https://mysterious-ravine-52687.herokuapp.com/'; //production
 const apiConfig = {
   headers: {
     'Content-Type': 'application/json',
