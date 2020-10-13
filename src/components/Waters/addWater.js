5980839c97dfc3ce0c6018c815c085949b322401
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2020-10-13 07:28:58 +0000

    styled add water component

--- src/components/Waters/addWater.js
+++ src/components/Waters/addWater.js
@@ -7,24 +7,25 @@ import styled from 'styled-components';
 import Footer from '../../pages/Footer';
 
 const AddWrap = styled.div`
-  height:470px;
-  wuser_idth: 60%;
+  height:360px;
+  width: 60%;
   margin-left: 20%;
 
-  @media (max-wuser_idth: 768px) {
-    height:450px;
-    wuser_idth: 100%;
+  @media (max-width: 768px) {
+    height:340px;
+    width: 100%;
     margin-left: 0;
   }
 `;
 
 const FormWrap = styled.form`
-  wuser_idth: 100%;
+  width: 100%;
   height: 300px;
+  margin-top: 50px;
 `;
 
 const AmountInp = styled.input`
-  wuser_idth: 100%;
+  width: 100%;
   padding: 12px 20px;
   margin: 8px 0;
   box-sizing: border-box;
@@ -36,7 +37,7 @@ const AmountInp = styled.input`
   }
 `;
 const TotalInp = styled.input`
-  wuser_idth: 100%;
+  width: 100%;
   padding: 12px 20px;
   margin: 8px 0;
   box-sizing: border-box;
@@ -49,12 +50,13 @@ const TotalInp = styled.input`
 `;
 
 const SubmitData = styled.button`
-  wuser_idth: 100%;
+  width: 100%;
   padding: 12px 20px;
   margin: 8px 0;
   box-sizing: border-box;
   border-radius: 4px;
-  background-color: light-blue;
+  background-color: red;
+  color: white;
 
   &:hover {
     cursor: pointer;
@@ -62,13 +64,21 @@ const SubmitData = styled.button`
 `;
 
 const TrackWater = styled.div`
-  text-align: center;
-  margin-top:40px;
   color: white;
   font-weight: 500;
   font-size: 23px;
+  text-align: center;
 `;
 
+const CenterW = styled.div`
+  width: 70%;
+  margin-left: 15%;
+
+  @media(max-width: 768px) {
+    display: flex;
+    margin-left: 15%;
+  }
+`;
 const AddWater = ({ addWaters }) => {
   const [formData, setFormData] = useState({
     amount: '',
@@ -89,42 +99,33 @@ const AddWater = ({ addWaters }) => {
   return (
     <>
     <AddWrap>
-      <div>
-        <div>
-          <TrackWater>
-            Add Water
-          </TrackWater>
+      <TrackWater>
+        Add Water
+      </TrackWater>
+      <CenterW>
+          
           <FormWrap onSubmit={onSubmit}>
-            <div>
-              <label htmlFor="amount">
-                <AmountInp
-                  type="number"
-                  name="amount"
-                  value={amount}
-                  onChange={onChange}
-                  placeholder="Amount"
-                  required
-                />
-              </label>
-            </div>
-            <div>
-              <label htmlFor="WaterTarget">
-                <TotalInp
-                  type="number"
-                  name="total"
-                  placeholder="Water Target"
-                  value={total}
-                  onChange={onChange}
-                  required
-                />
-              </label>
-            </div>
+            <AmountInp
+              type="number"
+              name="amount"
+              value={amount}
+              onChange={onChange}
+              placeholder="Amount"
+              required
+            />
+            <TotalInp
+              type="number"
+              name="total"
+              placeholder="Water Target"
+              value={total}
+              onChange={onChange}
+              required
+            />
             <SubmitData type="submit" onSubmit={onSubmit}>
               Add Water to Data
             </SubmitData>
           </FormWrap>
-        </div>
-      </div>
+      </CenterW>
     </AddWrap>
     <Footer />
     </>
